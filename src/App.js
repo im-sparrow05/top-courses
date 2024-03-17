61b86bbfb0adc64ae52f6f73f00d1a1479280b22
Rahul <rahulkr763386@gmail.com>
2024-03-17 20:19:19 +0530

    error handleding done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./Components/Navbar";
 import Filter from "./Components/Filter";
 import Cards from "./Components/Cards";
 import Spinner from "./Components/Spinner";
+import Erro from "./Components/Error";
+import NoData from "./Components/NoData";
 import { apiUrl, filterData } from "./data.js";
 import { toast } from "react-toastify";
 import { useEffect, useState } from "react";
@@ -11,16 +13,23 @@ const App = () => {
   const [courses,setCourses] = useState([]);
   const [loading,setLoading] = useState(true);
   const [category,setCategory] = useState('All');
+  const [error,setError] = useState(false);
+  const [noData,setNoData] = useState(false);
   useEffect(()=>{
     const fetchData = async() => {
       try{
         const res = await fetch(apiUrl);
+        if(!res){
+          setNoData(true);
+        }
         const output = await res.json();
         // save data into variable
         setCourses(output.data);
+        
 
       }catch{
-        toast.error("Network me koi dikkat hai");
+        toast.error("Error Occur While Fetching Api");
+        setError(true);
       }
       setLoading(false);
     }
@@ -40,7 +49,7 @@ const App = () => {
       />
       </div>
       <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
-      {loading ? (<Spinner/>): (<Cards courses={courses} category={category}/>)}
+      {loading ? (<Spinner/>): error?<Erro/>: noData?<NoData/>:(<Cards courses={courses} category={category}/>)}
       </div>
       </div>
     </div>
---
+++ src/Components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Erro = () => {
+  return (
+    <div className='text-white text-lg'>
+        Error 404
+    </div>
+  )
+}
+
+export default Erro
